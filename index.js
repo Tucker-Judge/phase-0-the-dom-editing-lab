1276aa6c25f1bb3d744a24fc140340f242852cba
Tucker <tudge669@gmail.com>
2022-11-27 18:09:57 -0500

    commit tree 99d99002fbfe7fd5977da82237baf3a14ccdf5b9

---
+++ index.js
@@ -0,0 +1,14 @@
+
+// const body = document.createElement("body")
+// document.append(body)
+// body.append(h1)
+const h1 = document.createElement("h1")
+h1.innerHTML = "My HTML adventure"
+document.body.append(h1)
+
+const p = document.createElement("p")
+document.body.append(p)
+const strongText = document.createElement("strong")
+strongText.innerHTML = "Strong text"
+p.append(strongText)
+
